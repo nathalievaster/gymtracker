f656c680bd81fe52c61fb54f7ecd03149f8cfa72
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-02-22 01:21:49 +0100

    träningspass

---
+++ Models/WorkoutExercises.cs
@@ -0,0 +1,19 @@
+namespace Gymtracker.Models
+
+public class WorkoutExercise
+{
+    public int Id { get; set; }
+    // Foreign Key till WorkoutSession
+
+    public int WorkoutSessionId { get; set; }
+    // Navigation property till WorkoutSession, så att vi kan få tillgång till all information om workout sessionen som övningen är kopplad till
+    public WorkoutSession WorkoutSession { get; set; } = default!;
+
+    public int ExerciseId { get; set; }
+    // Navigation property till Exercise, så att vi kan få tillgång till all information om övningen som är kopplad till workout sessionen
+    public Exercise Exercise { get; set; } = default!;
+
+    public List<SetEntry> Sets { get; set; } = new();
+    public List<ExerciseMedia> Media { get; set; } = new();
+
+}

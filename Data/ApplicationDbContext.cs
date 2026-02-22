0bc9c4fac25f354e91bffe4618eac7bba9e40d51
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-02-22 01:30:32 +0100

    Hade missat versal på Tracker

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using GymTracker.Models;
 
 namespace GymTracker.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<WorkoutSession> WorkoutSessions => Set<WorkoutSession>();
+    public DbSet<Exercise> Exercises => Set<Exercise>();
+    public DbSet<WorkoutExercise> WorkoutExercises => Set<WorkoutExercise>();
+    public DbSet<SetEntry> Sets => Set<SetEntry>();
+    public DbSet<ExerciseMedia> ExerciseMedia => Set<ExerciseMedia>();
 }
--- Models/WorkoutExercises.cs
+++ Models/WorkoutExercises.cs
@@ -1,19 +1,20 @@
-namespace Gymtracker.Models
-
-public class WorkoutExercise
+namespace GymTracker.Models
 {
-    public int Id { get; set; }
-    // Foreign Key till WorkoutSession
+    public class WorkoutExercise
+    {
+        public int Id { get; set; }
+        // Foreign Key till WorkoutSession
 
-    public int WorkoutSessionId { get; set; }
-    // Navigation property till WorkoutSession, så att vi kan få tillgång till all information om workout sessionen som övningen är kopplad till
-    public WorkoutSession WorkoutSession { get; set; } = default!;
+        public int WorkoutSessionId { get; set; }
+        // Navigation property till WorkoutSession, så att vi kan få tillgång till all information om workout sessionen som övningen är kopplad till
+        public WorkoutSession WorkoutSession { get; set; } = default!;
 
-    public int ExerciseId { get; set; }
-    // Navigation property till Exercise, så att vi kan få tillgång till all information om övningen som är kopplad till workout sessionen
-    public Exercise Exercise { get; set; } = default!;
+        public int ExerciseId { get; set; }
+        // Navigation property till Exercise, så att vi kan få tillgång till all information om övningen som är kopplad till workout sessionen
+        public Exercise Exercise { get; set; } = default!;
 
-    public List<SetEntry> Sets { get; set; } = new();
-    public List<ExerciseMedia> Media { get; set; } = new();
+        public List<SetEntry> Sets { get; set; } = new();
+        public List<ExerciseMedia> Media { get; set; } = new();
 
+    }
 }
